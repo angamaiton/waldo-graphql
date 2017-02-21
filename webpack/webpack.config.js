734d75f7bc9c95722ee0fe6c83a256d497461402
Kjartan Angamaiton <ofurmadur@icloud.com>
2017-02-21 12:37:18 -0600

    test

---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const vendors = require('./vendors');
+
+const config = {
+  entry: {
+    'dist/bundle': path.resolve(__dirname, '..', 'src', 'index.js'),
+    'dist/vendors': vendors
+  },
+  output: {
+    path: path.resolve(__dirname, '..', 'public'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'dist/vendors', filename: 'dist/vendors.bundle.js' }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+    }),
+  ],
+};
+
+module.exports = config;
